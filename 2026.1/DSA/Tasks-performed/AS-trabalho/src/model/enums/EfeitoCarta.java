f9a067abc4ffb8e05d9a4d9d2c1d93ca2c2ce33d
AlanSBauer <alanbauer00@gmail.com>
2026-06-30 23:50:05 -0300

    removendo projeto

--- 2026.1/DSA/Tasks-performed/AS-trabalho/src/model/enums/EfeitoCarta.java
+++
@@ -1,5 +0,0 @@
-package model.enums;
-
-public enum TipoCarta {
-    GANHAR_DINHEIRO, PERDER_DINHEIRO, AVANCAR, RETROCEDER, IR_PARA_PRISAO
-}
--- 2026.1/DSA/Tasks-performed/AS-trabalho/src/model/enums/TipoCasa.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum TipoCasa {
-    INICIO, IMOVEL, IMPOSTO, RESTITUICAO, PRISAO, LEILAO, SORTE_REVES
-}
